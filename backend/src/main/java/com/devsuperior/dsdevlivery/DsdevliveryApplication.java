29609c597633773a7478e46ad3ac44252dfd3dd2
s3c <gutherry.ferreira@gmail.com>
2021-02-20 17:52:08 -0300

    Project created

---
+++ backend/src/main/java/com/devsuperior/dsdevlivery/DsdevliveryApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsdevlivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsdevliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsdevliveryApplication.class, args);
+	}
+
+}
